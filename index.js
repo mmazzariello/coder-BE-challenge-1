f7422c012c687e045cf89cfd4f724fd404ce8d02
Mariana Mazza <mariana.mazzariello@bolttech.io>
2023-03-29 10:26:45 +0200

    ADD console.log to show that the methods work correctly

--- index.js
+++ index.js
@@ -32,3 +32,40 @@ class Product {
     this.stock = stock;
   }
 }
+
+const product1 = new Product(
+  "Product 1",
+  "Description 1",
+  10,
+  "thumbnail1.png",
+  "aaa123",
+  10
+);
+const product2 = new Product(
+  "Product 2",
+  "Description 2",
+  20,
+  "thumbnail2.png",
+  "aab123",
+  20
+);
+const product3 = new Product(
+  "Product 3",
+  "Description 3",
+  30,
+  "thumbnail3.png",
+  "aac123",
+  30
+);
+
+const productManager = new ProductManager();
+productManager.addProduct(product1);
+productManager.addProduct(product2);
+productManager.addProduct(product3);
+
+const productIdToFind = "mg5pgztww";
+const productById = productManager.getProductById(productIdToFind);
+console.log("productById", productById);
+
+console.log("productsArray", productManager.products);
+console.log("getProducts", productManager.getProducts());
